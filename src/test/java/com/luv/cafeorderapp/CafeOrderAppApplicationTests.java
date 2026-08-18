1023107fb63e4b9bb8a29d4d0d1264a6a665d94c
isu.craz <social.isu.craz@gmail.com>
2026-08-18 21:54:41 +0530

    chore: initialize Spring Boot project

---
+++ src/test/java/com/luv/cafeorderapp/CafeOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv.cafeorderapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeOrderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
